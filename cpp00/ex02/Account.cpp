44598ad5ec6b55f393254b022f5477d2ef660f8f
hohnuki <ohnukihiroki8585@yahoo.co.jp>
2022-06-14 11:12:33 +0900

    [feat]:cpp00ex02が概ね完了した

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -2,38 +2,83 @@
 // Created by 大貫　弘貴 on 2022/06/14.
 //
 
+#include <iostream>
+#include <iomanip>
 #include "Account.hpp"
 
 Account::Account(int initial_deposit) {
-	this->_nbDeposits = initial_deposit;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	this->_accountIndex = _nbDeposits;
+	this->_amount = initial_deposit;
+
+	_displayTimestamp();
+
+	std::cout << "index:" << this->_accountIndex << ";"
+			  << "amount:" << this->_amount << ";"
+			  << "created" << std::endl;
+}
+
+Account::~Account() {
+	_displayTimestamp();
+
+	std::cout << "index" << this->_accountIndex << ";"
+			  << "amount:" << this->_amount << ";"
+			  << "closed" << std::endl;
 }
 
 Account::Account() {}
 
-Account::~Account() {}
-
 void Account::displayAccountsInfos() {
+	_displayTimestamp();
 
+	std::cout << "accounts:" << getNbAccounts() << ";"
+		<< "total:" << getTotalAmount() << ";"
+		<< "deposits" << getNbDeposits() << ";"
+		<< "withdrawals:" << getNbWithdrawals() << std::endl;
 }
 
 void Account::makeDeposit(int deposit) {
+	this->_nbDeposits++;
+	this->_amount += deposit;
+	_displayTimestamp();
 
+	std::cout << "index:" << this->_accountIndex << ";"
+			  << "p_amount:" << this->_amount << ";"
+			  << "deposits:" << deposit << ";"
+			  << "amount:" << this->_amount << ";"
+			  << "nb_deposits:" << this->_nbDeposits << std::endl;
 }
 
 bool Account::makeWithdrawal(int withdrawal) {
+	_displayTimestamp();
 
+	std::cout << "index:" << this->_accountIndex << ";"
+			  << "p_amount:" << this->_amount << ";"
+			  << "withdrawal:" << withdrawal << ";"
+			  << "amount:" << this->_amount - withdrawal << ";"
+			  << "nb_withdrawals:" << this->_nbWithdrawals;
+	return true;
 }
 
 int Account::checkAmount() const {
-
+	return this->_amount;
 }
 
 void Account::displayStatus() const {
+	_displayTimestamp();
 
+	std::cout << "index:" << this->_accountIndex << ";"
+			<< "amount:" << this->_amount << ";"
+			<< "deposits:" << this->_nbDeposits << ";"
+			<< "withdrawals:" << this->_nbWithdrawals << ";"
+			<< std::endl;
 }
 
 void Account::_displayTimestamp() {
+	time_t currentTime = std::time(NULL);
 
+	std::cout << std::put_time(localtime(&currentTime), "[%Y%m%d_%H%M%S] ");
 }
 
 //getter
--- cpp00/ex02/tests.cpp
+++ cpp00/ex02/tests.cpp
@@ -21,21 +21,21 @@ int		main( void ) {
 
 	int	const				amounts[]	= { 42, 54, 957, 432, 1234, 0, 754, 16576 };
 	size_t const			amounts_size( sizeof(amounts) / sizeof(int) );
-	accounts_t				accounts( amounts, amounts + amounts_size );
-	accounts_t::iterator	acc_begin	= accounts.begin();
-	accounts_t::iterator	acc_end		= accounts.end();
+	accounts_t				accounts( amounts, amounts + amounts_size );//動的配列でamountsを持つ
+	accounts_t::iterator	acc_begin	= accounts.begin();//42を指すイテレータを持つ
+	accounts_t::iterator	acc_end		= accounts.end();//16576の次(?)を指すイテレータを持つ
 
 	int	const			d[]			= { 5, 765, 564, 2, 87, 23, 9, 20 };
 	size_t const		d_size( sizeof(d) / sizeof(int) );
-	ints_t				deposits( d, d + d_size );
-	ints_t::iterator	dep_begin	= deposits.begin();
-	ints_t::iterator	dep_end		= deposits.end();
+	ints_t				deposits( d, d + d_size );//動的配列でdを持つ
+	ints_t::iterator	dep_begin	= deposits.begin();//5を指すイテレータを持つ
+	ints_t::iterator	dep_end		= deposits.end();//20の次(?)を指すイテレータを持つ
 
 	int	const			w[]			= { 321, 34, 657, 4, 76, 275, 657, 7654 };
 	size_t const		w_size( sizeof(w) / sizeof(int) );
-	ints_t				withdrawals( w, w + w_size );
-	ints_t::iterator	wit_begin	= withdrawals.begin();
-	ints_t::iterator	wit_end		= withdrawals.end();
+	ints_t				withdrawals( w, w + w_size );//動的配列でwを持つ
+	ints_t::iterator	wit_begin	= withdrawals.begin();//321を指すイテレータを持つ
+	ints_t::iterator	wit_end		= withdrawals.end();//7654の次(?)を指すイテレータを持つ
 
 	Account::displayAccountsInfos();
 	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
