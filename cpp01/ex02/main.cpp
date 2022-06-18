6f3e1de79398d89333fc0e2d58819eba1b0ce05f
hohnuki <ohnukihiroki8585@yahoo.co.jp>
2022-06-18 17:31:33 +0900

    [feat]:cpp01ex02動いた

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by 大貫　弘貴 on 2022/06/18.
+//
+#include <iostream>
+
+int main() {
+	std::string str = "HI THIS IS BRAIN";
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
+
+	std::cout << "stringPTR Address: " << stringPTR << std::endl;
+	std::cout << "stringREF Address: " << &stringREF << std::endl;
+
+	std::cout << "stringPTR: " << *stringPTR << std::endl;
+	std::cout << "stringREF: " << stringREF << std::endl;
+}
