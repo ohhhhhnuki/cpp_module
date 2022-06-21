bec50082aff72492e8dfbcd163787a0f786b9f13
hohnuki <ohnukihiroki8585@yahoo.co.jp>
2022-06-21 10:37:44 +0900

    [feat]:cpp01ex04 雛形を書いた

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,3 +1,40 @@
 //
 // Created by 大貫　弘貴 on 2022/06/19.
 //
+#include <iostream>
+#include <fstream>
+
+std::string replaceOtherStr(std::string &replacedStr, std::string from, std::string to) {
+	const unsigned int pos = replacedStr.find(from);
+	const int len = from.length();
+
+	if (pos == std::string::npos || from.empty()) {
+		return replacedStr;
+	}
+
+	return replacedStr.replace(pos, len, to);
+}
+
+int main(int argc, char **argv) {
+	if (argc != 4)
+		std::exit(1);
+
+	try {
+		std::ifstream fis(argv[1]);
+		std::ofstream fos(std::strcat(argv[1], ".replace"), std::ios_base::app);
+
+		if (!fis)
+			std::cerr << "file can not open." << std::endl;
+		else {
+			while (true) {
+				std::string text;
+				getline(fis, text);
+				if (!fis)
+					break;
+				fos << replaceOtherStr(text, argv[2], argv[3]) << std::endl;
+			}
+		}
+	} catch (std::exception e) {
+		e.what();
+	}
+}
