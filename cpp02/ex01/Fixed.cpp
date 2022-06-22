f2f1b15e6f6fed5346c687b7461cfe18f0e33646
hohnuki <ohnukihiroki8585@yahoo.co.jp>
2022-06-22 10:08:11 +0900

    [add]:cpp02ex01の雛形作成した

---
+++ cpp02/ex01/Fixed.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 大貫　弘貴 on 2022/06/22.
+//
+
+#include "Fixed.h"
---
+++ cpp02/ex01/Fixed.h
@@ -0,0 +1,14 @@
+//
+// Created by 大貫　弘貴 on 2022/06/22.
+//
+
+#ifndef FIXED_H
+#define FIXED_H
+
+
+class Fixed {
+
+};
+
+
+#endif
