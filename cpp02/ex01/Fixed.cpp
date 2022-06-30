82e90d3f2c69674e499a263a91a2304e946e7eab
hohnuki <ohnukihiroki8585@yahoo.co.jp>
2022-06-30 21:54:58 +0900

    [feat]:cpp02ex01実装&動いた

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -2,4 +2,48 @@
 // Created by 大貫　弘貴 on 2022/06/22.
 //
 
+#include <cmath>
 #include "Fixed.h"
+
+Fixed::Fixed() {
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &fixed) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = fixed;
+}
+
+Fixed &Fixed::operator=(const Fixed &other) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	(void)other;
+	return *this;
+}
+
+Fixed::Fixed(int number) {
+	std::cout << "Int constructor called" << std::endl;
+	this->rawBits = number * (1 << Fixed::fractionalBits);
+}
+
+Fixed::Fixed(float number) {
+	std::cout << "Float constructor called" << std::endl;
+	this->rawBits = static_cast<int>(std::roundf(number * (1 << Fixed::fractionalBits)));
+}
+
+int Fixed::toInt() const{
+	return  this->rawBits / (1 << Fixed::fractionalBits);
+}
+
+float Fixed::toFloat() const{
+	return static_cast<float>(this->rawBits) / (1 << Fixed::fractionalBits);
+}
+
+std::ostream &operator<<(std::ostream &ost, Fixed const &fixed)
+{
+	ost << fixed.toFloat();
+	return ost;
+}
--- cpp02/ex01/Fixed.h
+++ cpp02/ex01/Fixed.h
@@ -5,10 +5,25 @@
 #ifndef FIXED_H
 #define FIXED_H
 
+#include <iostream>
 
 class Fixed {
+private:
+	int rawBits;
+	static const int fractionalBits = 8;
 
+public:
+	Fixed();
+	~Fixed();
+	Fixed(const Fixed& fixed);
+	Fixed(int number);
+	Fixed(float  number);
+	Fixed &operator=(const Fixed &other);
+
+	float toFloat(void) const;
+	int toInt(void) const;
 };
 
+std::ostream &operator<<(std::ostream &ost, Fixed const &fixed);
 
 #endif
