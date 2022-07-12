d11a6cef6b9a22f523b0ab2986e4db7700bfb4a7
hohnuki <ohnukihiroki8585@yahoo.co.jp>
2022-07-12 22:51:49 +0900

    [test]:cpp07ex00の雛形を転記した

---
+++ cpp07/ex00/whatever.h
@@ -0,0 +1,15 @@
+//
+// Created by 大貫　弘貴 on 2022/07/12.
+//
+
+#ifndef CPP_MODULE_WHATEVER_H
+#define CPP_MODULE_WHATEVER_H
+
+#include <iostream>
+
+class whatever {
+
+};
+
+
+#endif //CPP_MODULE_WHATEVER_H
