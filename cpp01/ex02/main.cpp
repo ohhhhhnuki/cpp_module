07c40a84764d761daab910904614c26c90bebf8c
hohnuki <ohnukihiroki8585@yahoo.co.jp>
2022-07-18 22:09:56 +0900

    [fix]:cpp01ex02のstringのアドレスと値を出力した

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -4,12 +4,17 @@
 #include <iostream>
 
 int main() {
-	std::string str = "HI THIS IS BRAIN";
-	std::string* stringPTR = &str;
-	std::string& stringREF = str;
+	std::string string = "HI THIS IS BRAIN";
+	std::string* stringPTR = &string;
+	std::string& stringREF = string;
 
+	std::cout << "string    address: " << &string << std::endl;
 	std::cout << "stringPTR address: " << stringPTR << std::endl;
 	std::cout << "stringREF address: " << &stringREF << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "string   : " << string << std::endl;
 	std::cout << "stringPTR: " << *stringPTR << std::endl;
 	std::cout << "stringREF: " << stringREF << std::endl;
 }
