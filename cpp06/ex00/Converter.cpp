e39afee421ea90e1b545d273872936382b2e4d30
hohnuki <ohnukihiroki8585@yahoo.co.jp>
2022-07-28 11:20:07 +0900

    [feat]:cpp06ex00 > getter&setterを実装した

--- cpp06/ex00/Converter.cpp
+++ cpp06/ex00/Converter.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "Converter.h"
+
+Converter::Converter(const std::string &str) : str(str) {}
+
+const std::string Converter::getType() const {
+	switch (valueType) {
+		case CHAR: return "Char";
+		case INT: return "Int";
+		case FLOAT: return "Float";
+		case DOUBLE: return "Double";
+		default : return "???????";
+	}
+}
